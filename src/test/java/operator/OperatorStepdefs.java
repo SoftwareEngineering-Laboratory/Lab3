2d6a94e8e2c9c78bed51a378614b48148411a88d
amirsadra.abdollahi <amirsadra.abdollahi@cafebazaar.ir>
2023-08-04 22:26:18 +0330

    Add test scenario and outline for operator calculating

--- src/test/java/operator/OperatorStepdefs.java
+++ src/test/java/operator/OperatorStepdefs.java
@@ -1,15 +1,17 @@
-package calculator;
+package operator;
 
+import calculator.Calculator;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
 
-public class MyStepdefs {
+public class OperatorStepdefs {
     private Calculator calculator;
     private int value1;
     private int value2;
+    private char opt;
     private int result;
 
     @Before
@@ -17,20 +19,20 @@ public class MyStepdefs {
         calculator = new Calculator();
     }
 
-    @Given("^Two input values, (-?\\d+) and (-?\\d+)$")
-    public void twoInputValuesAnd(int arg0, int arg1) {
+    @Given("^Two input values, (-?\\d+) and (-?\\d+), and input operator ([\\^*\\/])$")
+    public void twoInputValuesAnd(int arg0, int arg1, char arg2) {
         value1 = arg0;
         value2 = arg1;
-
+        opt = arg2;
     }
 
-    @When("^I add the two values$")
-    public void iAddTheTwoValues() {
-        result = calculator.add(value1, value2);
+    @When("^I apply operator on the two values$")
+    public void iApplyOptOnTheTwoValues() {
+        result = calculator.operator(value1, value2, opt);
         System.out.print(result);
     }
 
-    @Then("^I expect the result (-?\\d+)$")
+    @Then("^I expect the result (\\d+)$")
     public void iExpectTheResult(int arg0) {
         Assert.assertEquals(arg0, result);
     }
