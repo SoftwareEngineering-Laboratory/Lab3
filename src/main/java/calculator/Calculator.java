51b47ca61bb42c05400a34dded7a948a846792c0
amirsadra.abdollahi <amirsadra.abdollahi@cafebazaar.ir>
2023-08-04 22:25:41 +0330

    Add operator calculating in Calculator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,4 +4,30 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+
+
+    public int operator(int a, int b, char opt){
+        switch (opt){
+            case '*':
+                return multiply(a, b);
+            case '/':
+                return divide(a, b);
+            case '^':
+                return pow(a, b);
+            default:
+                return -1;
+        }
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public int pow(int a, int b) {
+        return (int)Math.pow(a, b);
+    }
 }
